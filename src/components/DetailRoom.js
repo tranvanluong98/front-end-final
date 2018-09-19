96f6d97c16f82d42ee2ce1f2741da74d5af9f9dc
Luong Tran <32104868+tranvanluong98@users.noreply.github.com>
2018-09-19 17:21:29 +0700

    Update DetailRoom.js

--- src/components/DetailRoom.js
+++ src/components/DetailRoom.js
@@ -36,19 +36,37 @@ class App extends Component {
   }
   render() {
     return (
-      <div className="DatailRoom">
+       <div className="DatailRoom">
         <div className="">
         <img style={{width:"150px"}} src="https://previews.123rf.com/images/leberus/leberus1709/leberus170900015/85171939-versus-%C3%A9cran-vs-lettres-match-de-comp%C3%A9tition-vs-match-bataille-martiale-vs-sport-.jpg" onClick={this._onChoose} className="img-detail"></img>
-        <span className="name-detail">{this.props.room.name}</span>
-        <span className="song-detail"> {this.props.room.nameSong}</span><br></br>
+        {/* Edit final */} 
+        <span className="name-detail"><h4 className="name-room-title">Tên Phòng:</h4><p className="room-name-custom">{this.props.room.name}</p></span>
+        <span className="song-detail"><h4 className="name-room-title">Tên Bài Hát: </h4> <p className="room-name-custom " style={{left: "157px",
+    top: "-35px"}}>{this.props.room.nameSong}</p></span><br></br>
+        {/* end */}
         <Link to={`InRoom/${this.props.room.name}/User`}>
           <button onClick={this._Xem} className="btn btn-warning btn-detail">Xem</button>
         </Link>
         {this.props.room.status == "Wait" ? <Link to={`InRoom/${this.props.room.name}/MainUser2`}>
-          <button onClick={this._Chien} className="btn btn-danger btn-detail">Chien</button>
+          <button onClick={this._Chien} className="btn btn-danger btn-detail">Chiến</button>
         </Link> : ''}
+
+        <hr/>
       </div>
       </div>
+//       <div className="DatailRoom">
+//         <div className="">
+//         <img style={{width:"150px"}} src="https://previews.123rf.com/images/leberus/leberus1709/leberus170900015/85171939-versus-%C3%A9cran-vs-lettres-match-de-comp%C3%A9tition-vs-match-bataille-martiale-vs-sport-.jpg" onClick={this._onChoose} className="img-detail"></img>
+//         <span className="name-detail">{this.props.room.name}</span>
+//         <span className="song-detail"> {this.props.room.nameSong}</span><br></br>
+//         <Link to={`InRoom/${this.props.room.name}/User`}>
+//           <button onClick={this._Xem} className="btn btn-warning btn-detail">Xem</button>
+//         </Link>
+//         {this.props.room.status == "Wait" ? <Link to={`InRoom/${this.props.room.name}/MainUser2`}>
+//           <button onClick={this._Chien} className="btn btn-danger btn-detail">Chien</button>
+//         </Link> : ''}
+//       </div>
+//       </div>
     );
   }
 }
